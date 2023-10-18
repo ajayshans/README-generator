5b909f98e3ad989a22945063e0c74f2a35f34466
Ajay S <ajaycshan@gmail.com>
2023-10-19 00:04:07 +1100

    renderLicenseBadge logic

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,22 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-    
+    switch(license) {
+        case 'MIT':
+            return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+            break;
+        case 'GPL 3.0':
+            return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+            break;
+        case 'Apache 2.0':
+            return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+            break;
+        case 'ISC':
+            return '[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)';
+            break;
+        case 'Unlicense':
+            return '';
+    }
 }
 
 // TODO: Create a function that returns the license link
