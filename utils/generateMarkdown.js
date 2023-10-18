e95f90d375ab909d9023c475ef892297168ffa5f
Ajay S <ajaycshan@gmail.com>
2023-10-19 00:19:39 +1100

    Remove TODO

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
     switch(license) {
@@ -19,7 +19,7 @@ function renderLicenseBadge(license) {
     }
 }
 
-// TODO: Create a function that returns the license link
+// Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
     switch(license) {
